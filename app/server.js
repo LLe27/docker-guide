26954afa9b7cd83a99216a1778de5d4b8f3d9131
Long Le <longle@Longs-MacBook-Pro.local>
2021-12-22 15:12:27 -0500

    beautified the app files

--- app/server.js
+++ app/server.js
@@ -11,12 +11,14 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get('/profile-picture', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.writeHead(200, {
+    'Content-Type': 'image/jpg'
+  });
   res.end(img, 'binary');
 });
 
@@ -27,7 +29,10 @@ let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 let mongoUrlDocker = "mongodb://admin:password@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+let mongoClientOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
 let databaseName = "my-db";
@@ -41,10 +46,16 @@ app.post('/update-profile', function (req, res) {
     let db = client.db(databaseName);
     userObj['userid'] = 1;
 
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
+    let myquery = {
+      userid: 1
+    };
+    let newvalues = {
+      $set: userObj
+    };
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+    db.collection("users").updateOne(myquery, newvalues, {
+      upsert: true
+    }, function (err, res) {
       if (err) throw err;
       client.close();
     });
@@ -62,7 +73,9 @@ app.get('/get-profile', function (req, res) {
 
     let db = client.db(databaseName);
 
-    let myquery = { userid: 1 };
+    let myquery = {
+      userid: 1
+    };
 
     db.collection("users").findOne(myquery, function (err, result) {
       if (err) throw err;
